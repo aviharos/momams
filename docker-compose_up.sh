f53c5285a9bcaa67cf97557202a71a2aec15c035
aviharos <vihand@protonmail.ch>
2022-08-25 18:47:38 +0200

    move notify cygnus curl command to separate script

--- docker-compose_up.sh
+++ docker-compose_up.sh
@@ -1,19 +1,1 @@
 docker-compose -p momams up -d
-curl --location --request POST 'http://localhost:1026/v2/subscriptions' \
---header 'Content-Type: application/json' \
---data-raw '{
-  "description": "Notify Cygnus Postgres of all context changes",
-  "subject": {
-    "entities": [
-      {
-        "idPattern": ".*"
-      }
-    ]
-  },
-  "notification": {
-    "http": {
-      "url": "http://cygnus:5055/notify"
-    }
-  }
-}
-'
